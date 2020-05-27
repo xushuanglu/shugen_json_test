de2e637cf4e87e089ccf852af3f775da67e238bd
xushuanglu <xushuanglu@datahunter.cn>
2020-05-27 15:43:12 +0800

    feature: commit doc file

--- controllers/code_test.go
+++ controllers/code_test.go
@@ -2,8 +2,17 @@ package controllers
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
+
+type data struct {
+	num    int
+	checks [10]func() bool   // 无法比较
+	doIt   func() bool       // 无法比较
+	m      map[string]string // 无法比较
+	bytes  []byte            // 无法比较
+}
 
 func TestCode(t *testing.T) {
 
@@ -50,5 +59,28 @@ func TestCode(t *testing.T) {
 	xRunes[0] = '我'
 	x1 = string(xRunes)
 	fmt.Println(x1) // 我ext
+
+	//range 迭代 string 得到的值
+	// data := "A\xfe\x02\xff\x04"
+	// for _, v := range data {
+	// 	fmt.Printf("%#x ", v) // 0x41 0xfffd 0x2 0xfffd 0x4    // 错误
+	// }
+
+	// for _, v := range []byte(data) {
+	// 	fmt.Printf("%#x ", v) // 0x41 0xfe 0x2 0xff 0x4    // 正确
+	// }
+
+	v1 := data{}
+	v2 := data{}
+	fmt.Println("v1 == v2: ", reflect.DeepEqual(v1, v2)) // true
+
+	m1 := map[string]string{"one": "a", "two": "b"}
+	m2 := map[string]string{"two": "b", "one": "a"}
+	fmt.Println("v1 == v2: ", reflect.DeepEqual(m1, m2)) // true
+
+	s1 := []int{1, 2, 3}
+	s2 := []int{1, 2, 3}
+	// 注意两个 slice 相等，值和顺序必须一致
+	fmt.Println("v1 == v2: ", reflect.DeepEqual(s1, s2)) // true
 
 }
